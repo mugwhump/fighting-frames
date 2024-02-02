2db7b0a82bac9783fe13329139aa2fc9b64850d1
Grayson Bartlet <grayson.bartlet@shaw.ca>
2024-02-01 17:42:34 -0800

    Clean up expired sessions

--- server/server.ts
+++ server/server.ts
@@ -22,6 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
+//import nodeUtil from 'util'; //node's util
+//logger.info("ENV vars: " + nodeUtil.inspect(process.env));
+//import * as Nano from 'nano';
+//const adminNano = Nano.default(`http://admin:password@`+process.env.COUCHDB_URL); 
+//const testfunc = async () => {
+  //let allDBs = await adminNano.db.list();
+  //logger.info("deebs = " + nodeUtil.inspect(allDBs));
+//}
+//testfunc();
+
 app.use('/', indexRouter);
 app.use('/api/v1', apiRouter);
 app.use('/auth', couchAuth.router);
@@ -37,6 +47,11 @@ app.listen(config.port, config.host, ()=> {
   logger.info(`*glomp* uwu ${config.name} running on ${config.host}:${config.port}`);
 });
 
-//TODO: run superlogin.removeExpiredKeys() every interval to remove expired sessions from _users
+//Run every 20m to remove expired sessions from _users
+setInterval(() => {
+  couchAuth.removeExpiredKeys().then((removedThings) => {
+    logger.info("Cleaned up expired sessions: " + removedThings.join(', '));
+  });
+}, 1200000);
 
 module.exports = app;
